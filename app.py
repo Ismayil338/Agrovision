71742e84340758d17087ff12af2c72365017d19f
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2025-11-30 19:56:37 +0400

    Update app.py

--- app.py
+++ app.py
@@ -200,4 +200,8 @@ if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+
